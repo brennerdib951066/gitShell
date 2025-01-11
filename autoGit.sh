1d55d97a5c2eb7d06fb25a514c93d7995fdc9182
brennerdib951066 <brennerbne@gmail.com>
2025-01-11 18:56:48 -0300

    Nova atualização

---
+++ autoGit.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+
+arquivoLogin="/usr/bin/dibScripts/shells/stable/bibliotecas/credenciais/credencial.sh"
+arquivoCor="/usr/bin/dibScripts/shells/stable/bibliotecas/cor/cores.txt"
+arquivoNotificacao="/usr/bin/dibScripts/shells/stable/bibliotecas/notificacao/notificarWhatsApp.txt"
+campanha='DIB'
+arquivoDeErro=~/"Área de Trabalho/erroGit"
+mensagemAviso=(
+    'Seus arquivos foram enviados com sucesso para o github',
+    'erro ao enviar arquivos para o github'
+)
+# Lista que receberá as pastas onde já tem todo o processo de git push configurado
+listaPastaPush=(
+    'gitAwk'
+    'gitMarkdown'
+    'gitPy'
+    'gitShell'
+    'gitVbs'
+)
+
+notificar(){
+        chaveApi='c7e572f0-c17b-4304-9478-b68641234d6c'
+        urlApi='https://backend.botconversa.com.br/api/v1/webhook/subscriber/subscriber_id/send_message/'
+        curl -X POST\
+        -H 'Content-Type: application/json'\
+        -H 'API-KEY: '${chaveApi}''\
+        -d "{\"type\": \"text\",\"value\": \"${2}\"}" "${urlApi/subscriber_id/${1}}"
+}
+
+# Função de enviar o push para o gitHub
+enviandoPush(){
+
+    cd ~/"Área de Trabalho/${1}" 2>>/"${arquivoDeErro}" && { git add .; git commit -m "Nova atualização"; git push origin main;} || notificar '385910829' "${mensagemAviso[1]^^} ${1}"
+    echo $i
+
+
+}
+
+for ((i=0;i<="${#listaPastaPush[@]}"-1;i++)) ; do
+
+    sleep 2s
+    enviandoPush "${listaPastaPush[i]}"
+    echo "${listaPastaPush[i]}"
+    #pwd
+done
