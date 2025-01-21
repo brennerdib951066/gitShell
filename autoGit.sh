4e90be5f97b3ae2e2b54fa8967e38ab8e702ae35
brennerdib951066 <brennerbne@gmail.com>
2025-01-21 14:24:27 -0300

    Nova atualização

--- autoGit.sh
+++ autoGit.sh
@@ -90,9 +90,10 @@ case "${dataAtual}" in
     *)
         diaData=$(awk 'BEGIN {FS="-";OFS="-"}{dia = int($1); print $1}' <<< "${dataAtual}")
         mesData=$(awk 'BEGIN {FS="-";OFS="-"}{mes = int($2); print $2}' <<< "${dataAtual}")
+	anoData=$(awk 'BEGIN {FS="-";OFS="-"}{ano = int($3); print $3}' <<< "${dataAtual}")
         #notificar '385910829' "\`Não se preocupe seu token git está de boa\`"
         [[ "${diaData}" -ge 20 && "${mesData}" -ge 2 ]] && notificar '385910829' "\`Seu token ${tokenAtual} expirou\`" ||\
-        notificar '385910829' "\`Token ainda é valido\` $diaData - $mesData"
+        notificar '385910829' "\`Token ainda é valido\` $diaData-$mesData-$anoData"
     ;;
 
 esac
