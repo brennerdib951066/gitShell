c5a38678c29295a1f56c666e27169fd2f1fcb631
brennerdib951066 <110617531+brennerdib951066@users.noreply.github.com>
2025-03-04 16:55:39 -0300

    Create instalarPowershell7.sh

---
+++ instalarPowershell7.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+
+arquivoLogin="/usr/bin/dibScripts/shells/stable/bibliotecas/credenciais/credencial.sh"
+arquivoCor="/usr/bin/dibScripts/shells/stable/bibliotecas/cor/cores.txt"
+arquivoNotificacao="/usr/bin/dibScripts/shells/stable/bibliotecas/notificacao/notificarWhatsApp.txt" # SINTAXE | notificar '385910829' "Mensagem, texto" |
+
+# Download the powershell '.tar.gz' archive
+curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v7.5.0/powershell-7.5.0-linux-x64.tar.gz
+
+# Create the target folder where powershell will be placed
+sudo mkdir -p /opt/microsoft/powershell/7
+
+# Expand powershell to the target folder
+sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
+
+# Set execute permissions
+sudo chmod +x /opt/microsoft/powershell/7/pwsh
+
+# Create the symbolic link that points to pwsh
+sudo ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
