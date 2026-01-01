2c257e21eaca473ae1efe3af7676a7f3a64bb8ce
brennerdib951066 <110617531+brennerdib951066@users.noreply.github.com>
2026-01-01 14:15:42 -0300

    Update digitarEmail.sh

--- digitarEmail.sh
+++ digitarEmail.sh
@@ -1,35 +1,34 @@
 #!/usr/bin/env -S bash
 
-# Version: 1.0.1
+# Version: 1.0.3
 
 #setxkbmap -layout br
 
 # Testando python
 areaDeTrabalho="$(xdg-user-dir DESKTOP)"
 diretorioPythonBase="$areaDeTrabalho/gitPy"
+plataforma=$(awk 'BEGIN {FS="="}$1 ~ /^ID$/ {plataforma = tolower($2); print plataforma}' /etc/os-release)
+# status=(
+#     'desligouNaCara'
+#     'jaTemPlano'
+#     'contatoErrado'
+#     'mudo'
+#     'semInteresse'
+#     'atendeu'
+#     'listaNegra'
+# )
+#
+# comentarios=(
+#     'desligou na cara'
+#     'ja tem plano'
+#     'contato errado'
+#     'mudo'
+#     'sem interesse'
+#     'atendeu'
+#     'lista Negra, nunca ligar!'
+# )
 
-status=(
-    'desligouNaCara'
-    'jaTemPlano'
-    'contatoErrado'
-    'mudo'
-    'semInteresse'
-    'atendeu'
-    'listaNegra'
-)
-
-comentarios=(
-    'desligou na cara'
-    'ja tem plano'
-    'contato errado'
-    'mudo'
-    'sem interesse'
-    'atendeu'
-    'lista Negra, nunca ligar!'
-)
-
-declare -A testeDimensional
-testeDimensional=(
+declare -A testeDimensional=(
     [desligouNaCara]='desligou na cara'
     [jaTemPlano]='ja tem plano'
     [contatoErrado]='contato errado'
@@ -39,12 +38,47 @@ testeDimensional=(
     [listaNegra]='lista Negra, nunca ligar!'
 )
 
+# Verificando se o usuario esta como root
+((UID==0)) && { echo $'\E[31;1mNão usa-me como admistrador!\E[m'; exit ;}
+
 # Verificando se existe paramtro na linha de comando
 
-[[ "${1:?$(echo $'\E[31;2mPor favor envie paramentro 1\E[m')}" ]] || exit
+[[ "${1:?$(echo -e $'\E[31;1mPor favor use um desses parametros:\E[m\n\E[31;2m'${!testeDimensional[@]}'\E[m\E[m')}" ]] || exit
+
+# Verificando se o diretorio padrao do PY existe!
+[[ -d "${diretorioPythonBase}" ]] || { echo $'\E[31;1mO diretorio base do python não está no PC!\E[m'; exit; }
+
+[[ "${plataforma}" ]] || { echo -e "\E[34;1mNão achei no arquivo realease o tipo de sistema!\E[m"; exit ;}
+
+# Vericando se o xdotool esta instalado
+type -P 'xdotool' >/dev/null || {
+    printf '%b %s' '\e[34;1mO XDOTOOL não está instalado\e[m'
+    read -p 'INSTALAR[s/n]: ' instalarXdotool
+    [[ ${instalarXdotool} ]] || instalarXdotool='s'
+    case "${instalarXdotool}" in
+        's')
+            [[ "${plataforma}" =~ ubuntu|debian ]] && {
+                sudo apt install xdotool
+                clear
+            }
+        ;;
+        'n')
+            echo "Preciso do xdotool instalado!"
+            exit
+        ;;
+        *)
+            echo "Escolha S para instalar ou n para sair"
+            exit
+        ;;
+    esac
+}
+
 
 # Verificando se existe o primeiro argumento mandado na lista correspondente a status
-grep -qiwE "${1}" <<< ${status[@]} || { echo $'\E[37;1mEnvie como paramentro ${1}: ['"${status[@]}"']'; exit ;}
+grep -qiwE "${1}" <<< ${!testeDimensional[@]} || {
+    echo $'\E[37;1mEnvie como paramentro ${1}: ['"${!testeDimensional[@]}"']'
+    exit
+}
 
 sleep 1s
 
@@ -54,25 +88,3 @@ for ((i=0;i<"${#testeDimensional[@]}";i++)); do
 
     [[ "${dimensaoAtual}" == ${1} ]] && { xdotool type --delay 150 "# ${testeDimensional[$dimensaoAtual]^}" ; python3 "$diretorioPythonBase/clicarStatus.py" "${dimensaoAtual}" "0.7" ; }
 done
-
-# for ((i=0;i<${#status[@]};i++)) ; do
-#     #echo -e "Aqui é a lista: ${status[i]}"
-#
-#     # Se o comentario conter o semInteresse ele escreve
-#     [[ "${status[i]}" == ${status[4]} && "${1}" == ${status[4]} ]] && { xdotool type --delay 150 "# ${comentarios[i]^}" ; python3 "$diretorioPythonBase/clicarStatus.py" "${status[i]}" "0.8" ; continue;}
-#
-#     [[ "${1}" == ${status[i]} ]] && { xdotool type --delay 150 "# ${comentarios[i]^}"
-#     python3 "$diretorioPythonBase/clicarStatus.py" "${status[i]}" "0.7" ; }
-#
-#     sleep 0.5s
-#
-# done
-
-# xdotool type --delay 100 "brennerbne@gmail.com"
-# sleep 2
-# xdotool key Return
-
-
-
-
-
