1953ed0cdc46dd961a28fe78203f888b81de30e3
brennerdib951066 <110617531+brennerdib951066@users.noreply.github.com>
2026-01-04 16:24:52 -0300

    Update 3cplusRelatorio.sh

--- 3cplusRelatorio.sh
+++ 3cplusRelatorio.sh
@@ -1,4 +1,4 @@
-curl -X GET "https://3c.fluxoti.com/api/v1/campaigns/229993/agents/qualifications?start_date=2025-12-01&end_date=2025-12-31&api_token=0ThoHgXPfj3rFp6EZdVc1811q8u7ujL4QcCdvJYOpi1AYe77dC2qIh1jQlbS" -H "accept: application/json" | jq -r '.data.agents[]
+(echo "Qualificação,Total" ;curl -X GET "https://3c.fluxoti.com/api/v1/campaigns/229993/agents/qualifications?start_date=2025-12-01&end_date=2025-12-31&api_token=0ThoHgXPfj3rFp6EZdVc1811q8u7ujL4QcCdvJYOpi1AYe77dC2qIh1jQlbS" -H "accept: application/json" | jq -r '.data.agents[]
   | select(.name | contains("Brenner"))
   | .qualifications[]
-  | "\(.name) : \(.total)"'
+  | [.name,.total] | @csv') > 3cplus.csv
