c95e6004aeb0213d274614908cf1fe51428c7f32
brennerdib951066 <110617531+brennerdib951066@users.noreply.github.com>
2026-01-04 18:59:59 -0300

    Update 3cplusRelatorio.sh

--- 3cplusRelatorio.sh
+++ 3cplusRelatorio.sh
@@ -1,4 +1,11 @@
-(echo "Qualificação,Total" ;curl -X GET "https://3c.fluxoti.com/api/v1/campaigns/229993/agents/qualifications?start_date=2025-12-01&end_date=2025-12-31&api_token=0ThoHgXPfj3rFp6EZdVc1811q8u7ujL4QcCdvJYOpi1AYe77dC2qIh1jQlbS" -H "accept: application/json" | jq -r '.data.agents[]
+quantidade=$(curl -sX GET 'https://3c.fluxoti.com/api/v1/campaigns/229993/agents/qualifications?start_date=2025-12-01&end_date=2025-12-31&api_token=0ThoHgXPfj3rFp6EZdVc1811q8u7ujL4QcCdvJYOpi1AYe77dC2qIh1jQlbS' -H 'accept: application/json' | jq -r '.data.agents[] | select(.name | contains("Brenner")) | .calls')
+
+qtdStatus=$(echo \*\"QUALIFICAÇÃO\*\"","\*\"TOTAL\*\" ;curl -sX GET "https://3c.fluxoti.com/api/v1/campaigns/229993/agents/qualifications?start_date=2025-12-01&end_date=2025-12-31&api_token=0ThoHgXPfj3rFp6EZdVc1811q8u7ujL4QcCdvJYOpi1AYe77dC2qIh1jQlbS" -H "accept: application/json" | jq -r '.data.agents[]
   | select(.name | contains("Brenner"))
   | .qualifications[]
-  | [.name,.total] | @csv') > 3cplus.csv
+  | [.name,.total] | @csv') #> 3cplus.csv
+
+qtdStatus="${qtdStatus//,/= }"
+quebraDeLInha=$'\n'
+pwsh -File "/mnt/c/Users/brenner/Desktop/gitPowershell/notificarWhatsApp.ps1" "notificarWhatsApp" "${qtdStatus//\"/}${quebraDeLInha}${quebraDeLInha} \`TOTAL : ${quantidade}\`" "brenner"
+
